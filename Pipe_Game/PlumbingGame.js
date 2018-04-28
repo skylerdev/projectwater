918beb4ffd5d1322bb4fbaaff9a08230baf9ab2e
Adam King <aking58@my.bcit.ca>
2018-04-28 13:06:39 -0700

    Added Early Pipe Game
    No functionality currently

---
+++ Pipe_Game/PlumbingGame.js
@@ -0,0 +1,9 @@
+function setupGrid(){
+	var canvas = document.getElementById("gameDisplay");
+}
+function drawGrid(){
+	var canvas = document.getElementById("gameDisplay");
+	var ctx = canvas.getContext("2d");
+	ctx.save();
+	ctx.beginPath();
+}
