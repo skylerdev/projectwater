41799ad59b5e38fde0f96f3747eeae491cbe4aec
John Lee <johnlee46@gmail.com>
2018-05-01 13:53:09 -0700

    add checkcookie
    checkcookie

--- Login_Page/makecookie.js
+++ Login_Page/makecookie.js
@@ -1,7 +1,16 @@
 function makeCookie() {
+  var cookieExist = checkCookie();
+    if(!cookieExist){
         var d = new Date();
         d.setTime(d.getTime() + (1*24*60*60*1000));
         var expires = "expires="+ d.toUTCString();
         document.cookie = "username" + "=" + myForm.username.value + ";" + expires + ";path=/";
         document.cookie = "email" + "=" + myForm.email.value + ";" + expires + ";path=/";
+    }
       }
+
+function checkCookie(){
+  if (document.cookie.indexOf("visited=")>= 0){
+    return true;
+  }
+}
