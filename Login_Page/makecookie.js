7e9fba0e73defd59bd679001b928a1b5c1d77ddd
Zach Barker <zbarker1@my.bcit.ca>
2018-05-02 10:34:26 -0700

    signup page now responsive

--- Login_Page/makecookie.js
+++ Login_Page/makecookie.js
@@ -1,10 +1,20 @@
 function makeCookie() {
+  var cookieExist = checkCookie();
+    if(!cookieExist){
         var d = new Date();
         d.setTime(d.getTime() + (1*24*60*60*1000));
         var expires = "expires="+ d.toUTCString();
         document.cookie = "username" + "=" + myForm.username.value + ";" + expires + ";path=/";
         document.cookie = "email" + "=" + myForm.email.value + ";" + expires + ";path=/";
+<<<<<<< HEAD
+    }
       }
+
+function checkCookie(){
+  if (document.cookie.indexOf("visited=")>= 0){
+    return true;
+  }
+}
 
 var password = document.getElementById("password")
   , confirm_password = document.getElementById("confirm_password");
@@ -19,3 +29,6 @@ function validatePassword(){
 
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
+=======
+      }
+>>>>>>> 9a90572b5c23a68a8f28938973aa0507d845d90f
