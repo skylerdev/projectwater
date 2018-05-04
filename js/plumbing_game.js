cd766517c9bfb0bcf29809014bf61d4820dec4ea
Adam King <aking58@my.bcit.ca>
2018-05-04 13:39:31 -0700

    Removed outdated files

--- js/plumbing_game.js
+++
@@ -1,9 +0,0 @@
-function setupGrid(){
-	var canvas = document.getElementById("gameDisplay");
-}
-function drawGrid(){
-	var canvas = document.getElementById("gameDisplay");
-	var ctx = canvas.getContext("2d");
-	ctx.save();
-	ctx.beginPath();
-}
