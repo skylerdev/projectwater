926195cdd43b848b7a77f60ee5a88e112555df5f
Adam King <aking58@my.bcit.ca>
2018-05-11 11:38:26 -0700

    Pipe game update
    Implemented database communication
    Did minor game balancing to ensure that losing intentionally from the start wouldnt net you a score similar to if you won.

--- assets/Pipe_Sheet_Transparent.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Pipe_Sheet_Transparent" tilewidth="64" tileheight="64" tilecount="7" columns="7">
- <image source="../../PipeGame/Assets/Pipe_Sheet_Transparent.png" width="448" height="64"/>
-</tileset>
