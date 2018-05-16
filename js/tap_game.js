911ee64bb537c209517f6184c8e0f0d62a1e0e50
skylerdev <Skyler2@mac.com>
2018-05-16 11:52:57 -0700

    time, score, drip

--- js/tap_game.js
+++ js/tap_game.js
@@ -3,6 +3,18 @@ var game = new Phaser.Game(500, 800, Phaser.AUTO, 'tap-game');
 var newWidth;
 var newHeight;
 var paused = false;
+var gameLength = 30;
+var currentScore = 0;
+var tapArray = [];
+var kills = 0;
+
+var dripFreq = 3;
+var showDripFreq = 1;
+
+var font = {
+    font: '25px Times New Roman',
+    fill: '#ff00ff'
+};
 
 //boot
 var bootState = {
@@ -20,106 +32,107 @@ var bootState = {
 var loadState = {
     preload: function () {
 
-        var loadingLabel = game.add.text(newWidth / 2, newHeight / 2, 'LOADING...', {
-            font: '20px Comic Sans'
-            , fill: '#ff0000'
-        });
+        var loadingLabel = game.add.text(newWidth / 2, newHeight / 2, 'LOADING...', font);
 
         //ALL ASSETS LOADED HERE
-
         game.load.image("valve", "assets/valve.png");
         game.load.image("drop", "assets/drop.png");
         game.load.image("play", "assets/play.png");
-
+        game.load.image('black', "assets/black.png");
 
     },
     create: function () {
 
-     var done = game.add.text(newWidth/2 +100  , newHeight/2 + 100, 'DONE :)');
-     game.state.start('menu');
-
+        var loadingLabel = game.add.text(200, 400, 'DONE...', font);
+        game.state.start('menu');
     }
 }
 
-//menu
+//men
 var menuState = {
     create: function () {
         game.stage.backgroundColor = "#2F95AA";
 
-        var titleMain = game.add.text(game.canvas.width / 2, game.canvas.height / 2, 'WHACK A TAP', {
-            font: '25 px Times New Roman',
-            fill: '#ff00ff'
-        });
+        var titleMain = game.add.text(200, 400, 'WHACK A TAP', font);
         var playButton = game.add.button(0, 0, 'play', pressedPlay, this);
 
-    },
+    }
 }
-
-//variables for playstate
-var time = 0;
-
 
 //PLAY that funky music white boy
 var playState = {
     create: function () {
 
             //create
+            var topBarY = newHeight / 6;
+            var rest = newHeight - topBarY;
+
             for(var i = 0; i < 3; i++) for (var j = 0; j < 4; j++) {
-                //add
-                var image = game.add.sprite(0, 0, 'valve');
-                //calc and set pos
-                var x = i*image.width;
-                var y = j*image.height;
-                image.x = x;
-                image.y = y;
 
-                image.inputEnabled = true;
-                image.events.onInputDown.add(clickListener, this);
+                var x = (i * newWidth/3) + 10;
+                var y = (j * rest / 4) + topBarY;
+                var button = game.add.button(x, y, 'valve', buttonEvent, this);
+                button.dripping = false;
 
+                tapArray.push(button);
 
             }
-
-            //pausebutton
-            var pause = game.add.button(0, 0, 'valve', pressedPause, this);
-            pause.width = 50;
-            pause.height = 50;
-            pause.x = game.width-50;
-            pause.y = game.height-50;
-
-
+             game.time.events.loop(dripFreq * 999, startDrip, this);
+             game.time.events.loop(showDripFreq * 1000, showDrops, this);
             game.stage.backgroundColor = "#2F95AA";
+
+            //end
+            game.time.events.add(1000 * gameLength, endGame, this);
+
 
 
     },
-    update: function () {
-        time++;
-        if(time > 500){
-            game.state.start('done');
-        }
-
+    render: function () {
+        game.debug.text("");
     }
 }
 
 var doneState = {
     create: function() {
+        game.stage.backgroundColor = "#2F95AA";
+
+        var titleMain = game.add.text(200, 400, 'YOURE DONE KID', font);
+        var titleTwo = game.add.text(100, 400, kills + ' VALVES GOTTEN', font);
+        var playButton = game.add.button(100, 600, 'play', pressedMenu, this);
 
     }
 }
 
-function pauseGame() {
-
-
-}
-
-function clickListener() {
-    var drop = game.add.sprite(0, 0, 'drop');
-
-    game.physics.enable(drop, Phaser.Physics.ARCADE);
-    drop.body.bounce.y = 0.1;
-    drop.body.collideWorldBounds = true;
-    drop.body.gravity.y = 200;
+function startDrip() {
+    //chose a random one to start dripping.
+    var randNum = Math.floor(Math.random() * tapArray.length);
+    tapArray[randNum].dripping = true;
 
 }
+
+function buttonEvent() {
+    kills++;
+}
+
+function showDrops() {
+    var i;
+    for(i = 0; i < tapArray.length; i++){
+        if(tapArray[i].dripping){
+            var drop = game.add.sprite(tapArray[i].x, tapArray[i].y, 'drop');
+
+            game.physics.enable(drop, Phaser.Physics.ARCADE);
+            drop.body.bounce.y = 0.1;
+            drop.body.collideWorldBounds = false;
+            drop.body.gravity.y = 200;
+        }
+    }
+}
+
+function endGame() {
+    game.state.start('done');
+}
+
+
 
 function resize() {
     var canvas = game.canvas
@@ -145,6 +158,9 @@ var pressedPause = function() {
     paused = true;
 }
 
+var pressedMenu = function() {
+    game.state.start('menu');
+}
 
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
