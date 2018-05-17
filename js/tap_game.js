3b9981ed226603a80912e5dfefe9a209afe133fc
skylerdev <Skyler2@mac.com>
2018-05-17 12:12:55 -0700

    Tap MVP

--- js/tap_game.js
+++ js/tap_game.js
@@ -3,7 +3,7 @@ var game = new Phaser.Game(500, 800, Phaser.AUTO, 'tap-game');
 var newWidth;
 var newHeight;
 var paused = false;
-var gameLength = 1;
+var gameLength = 45;
 var tapArray = [];
 var kills = 0;
 var score = 0;
@@ -12,8 +12,8 @@ var dripFreq = 3;
 var showDripFreq = 1;
 
 var font = {
-    font: '25px Times New Roman',
-    fill: '#ff00ff'
+    font: '50px Times New Roman',
+    fill: '#f5a3ff'
 };
 
 //boot
@@ -31,7 +31,6 @@ var bootState = {
 //load
 var loadState = {
     preload: function () {
-
         var loadingLabel = game.add.text(newWidth / 2, newHeight / 2, 'LOADING...', font);
 
         //ALL ASSETS LOADED HERE
@@ -40,14 +39,15 @@ var loadState = {
         game.load.image("play", "assets/play.png");
         game.load.image('black', "assets/black.png");
         game.load.image('twitter', "assets/tweet.png");
-        game.load.audio('')
+        game.load.audio('drip0', "assets/drip6.mp3");
+        game.load.audio('drip1', "assets/drip1.mp3");
+        game.load.audio('drip2', "assets/drip2.mp3");
+        game.load.audio('drip3', "assets/drip3.mp3");
+        game.load.audio('drip4', "assets/drip4.mp3");
+        game.load.audio('drip5', "assets/drip5.mp3");
         //TODO: more graphics
-
-
     },
     create: function () {
-
-        var loadingLabel = game.add.text(200, 400, 'DONE...', font);
         game.state.start('menu');
     }
 }
@@ -57,8 +57,8 @@ var menuState = {
     create: function () {
         game.stage.backgroundColor = "#2F95AA";
 
-        var titleMain = game.add.text(200, 400, 'WHACK A TAP', font);
-        var playButton = game.add.button(0, 0, 'play', pressedPlay, this);
+        var titleMain = game.add.text(10, 100, 'WHACK A TAP', font);
+        var playButton = game.add.button(0, 2*newHeight/5, 'play', pressedPlay, this);
 
 
     }
@@ -73,30 +73,30 @@ var playState = {
             var rest = newHeight - topBarY;
 
             for(var i = 0; i < 3; i++) for (var j = 0; j < 4; j++) {
-
                 var x = (i * newWidth/3) + 10;
                 var y = (j * rest / 4) + topBarY;
-
                 var button = game.add.button(x, y, 'valve', buttonEvent, this);
                 button.dripping = false;
                 tapArray.push(button);
-
             }
+
+            //start drip timers
              game.time.events.loop(dripFreq * 999, startDrip, this);
              game.time.events.loop(showDripFreq * 1000, makeDrops, this);
             game.stage.backgroundColor = "#2F95AA";
 
-            //end
+            //end condition
             game.time.events.add(1000 * gameLength, endGame, this);
 
 
 
     },
     update: function() {
+        //TODO: if at any time more than 5 taps dripping, loss condition
 
     },
     render: function () {
-        game.debug.text("");
+        var scoreText = game.add.text(10, 10, "SCORE: " + kills, font);
     }
 }
 
@@ -105,10 +105,28 @@ var doneState = {
     create: function() {
         game.stage.backgroundColor = "#2F95AA";
 
-        var titleMain = game.add.text(200, 200, 'YOURE DONE KID', font);
-        var titleTwo = game.add.text(200, 600, kills + ' VALVES GOTTEN', font);
-        var playButton = game.add.button(100, 600, 'play', pressedMenu, this);
-        var tweetbutton = game.add.button(0, 700, 'twitter', tweetMe, this);
+        var score = kills;
+
+        var titleMain = game.add.text(newWidth/10, newHeight/5, 'YOURE DONE KID', font);
+        var titleTwo = game.add.text(newWidth/10, 2*newWidth/5, kills + ' VALVES GOTTEN', font);
+        var titleTwo = game.add.text(newWidth/10, 3*newWidth/5, 'congrats... bro.. ', font);
+        var titleThree = game.add.text(newWidth/10, 4*newWidth/5, 'U SAVED WOTER', font);
+        var tweetbutton = game.add.button(newWidth/10, newWidth, 'twitter', tweetMe(score), this);
+
+        var username = getCookie("username");
+
+        //highscore
+        $.ajax({
+            method: "POST",
+            url: "tapphp.php",
+            data: {username: username,
+                score: score},
+            dataType: "text",
+            success:function(data){
+                console.log(data);
+            }
+
+        });
 
     }
 }
@@ -138,10 +156,22 @@ function makeDrops() {
 
             game.physics.enable(drop, Phaser.Physics.ARCADE);
             drop.body.bounce.y = 0.1;
-            drop.body.collideWorldBounds = false;
+            drop.body.collideWorldBounds = true;
             drop.body.gravity.y = 200;
+
+            drop.body.onWorldBounds = new Phaser.Signal();
+            drop.body.onWorldBounds.add(hitWorldBounds, this);
+
         }
     }
+}
+
+function hitWorldBounds() {
+    arguments[0].destroy();
+    var randNum = Math.floor(Math.random() * 5);
+    var drippy = game.add.audio('drip' + randNum);
+    drippy.allowMultiple = true;
+    drippy.play();
 }
 
 function endGame() {
@@ -164,10 +194,26 @@ function resize() {
     }
 }
 
-function tweetMe() {
+function tweetMe(score) {
     var twittertext = 'My Whack a Tap score today was ' + score + '! Try to beat me at http://project-water.ca.';
     var outTweet = 'http://twitter.com/home?status=' + twittertext;
     window.open(outTweet, '_blank');
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 //TODO: fadeblack effect
